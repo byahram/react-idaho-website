f46bc34d1edd186ff97d87f1bfe17c83648efec2
Ahram-Kim <ahram0223@gmail.com>
2022-09-09 09:49:22 +0900

    Done with Responsive Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -116,6 +116,39 @@ function Card(props) {
             </a>
           </div>
         </article>
+        <article className="card">
+          <figure className="card__header">
+            <img src={hotSpring} alt="hot spring" />
+          </figure>
+          <div className="card__body">
+            <h3 className="tit">HOT SPRING</h3>
+            <p className="desc">
+              you’ll find Idaho’s beloved hot springs waiting to wash away your
+              stress. For an alternative soaking experience, visit any of the
+              more developed hot springs spots for a soak with modern amenities.
+            </p>
+            <a
+              className="btn"
+              href="https://visitidaho.org/things-to-do/hot-springs/"
+            >
+              view more
+              <span aria-hidden="true">
+                <svg
+                  width="52"
+                  height="8"
+                  viewBox="0 0 52 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M51.3536 4.35355C51.5488 4.15829 51.5488 3.84171 51.3536 3.64645L48.1716 0.464466C47.9763 0.269204 47.6597 0.269204 47.4645 0.464466C47.2692 0.659728 47.2692 0.976311 47.4645 1.17157L50.2929 4L47.4645 6.82843C47.2692 7.02369 47.2692 7.34027 47.4645 7.53553C47.6597 7.7308 47.9763 7.7308 48.1716 7.53553L51.3536 4.35355ZM0 4.5H51V3.5H0V4.5Z"
+                    fill="#5B5B5B"
+                  />
+                </svg>
+              </span>
+            </a>
+          </div>
+        </article>
       </div>
     </section>
   );
