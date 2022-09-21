ec896cbba2038658bd3729fca53f276a74c21a4b
Ahram-Kim <ahram0223@gmail.com>
2022-09-21 14:46:39 +0900

    2022.09.21 Header.jsx slider.css bug fixed 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,7 @@ function Header(props) {
         </div>
 
         <nav className="header__menu">
-          <ul class="active">
+          <ul>
             {menuText.map((txt) => (
               <MenuWrap text={txt.menu} key={txt.menu} goto={txt.goto} />
             ))}
